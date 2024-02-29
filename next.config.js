b3adf5382eab53bb04243763924b3e1be12b1661
Jack Hollis-London <me@jh0l.com>
2024-02-29 15:13:09 +1100

    Add and delete manifest files

--- next.config.js
+++ next.config.js
@@ -15,10 +15,10 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
  * A fork of 'next-pwa' that has app directory support
  * @see https://github.com/shadowwalker/next-pwa/issues/424#issuecomment-1332258575
  */
-// const withPWA = require('@ducanh2912/next-pwa').default({
-//   dest: 'public',
-//   disable: process.env.NODE_ENV === 'development',
-// })
+const withPWA = require('@ducanh2912/next-pwa').default({
+  dest: 'public',
+  // disable: process.env.NODE_ENV === 'development',
+})
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -83,10 +83,7 @@ const nextConfig = {
 const KEYS_TO_OMIT = ['webpackDevMiddleware', 'configOrigin', 'target', 'analyticsId', 'webpack5', 'amp', 'assetPrefix']
 
 module.exports = (_phase, { defaultConfig }) => {
-  const plugins = [
-    // [withPWA],
-    [withBundleAnalyzer, {}],
-  ]
+  const plugins = [[withPWA], [withBundleAnalyzer, {}]]
 
   const wConfig = plugins.reduce((acc, [plugin, config]) => plugin({ ...acc, ...config }), {
     ...defaultConfig,
